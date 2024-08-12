2bf391785b1341979473273e88b808adcce85c77
Naga Anjaneyulu Karumuri <anjaneyulun02@gmail.com>
2024-08-12 06:24:16 +0000

    updated routing to only use app routing and fixed firebase auth state management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,37 @@
 // app/layout.tsx
+
+'use client';
+
 import './globals.css';  // Global styles
 import { Inter } from 'next/font/google';  // Google font
 import { ChakraProvider } from '@chakra-ui/react';  // Chakra UI provider
+import { useEffect } from 'react';
+import { initializeFirebase } from './firebase';  // Adjust the import path as necessary
+import ErrorBoundary from './components/ErrorBoundary';  // Error boundary component
+import Head from 'next/head';  // Import Head component
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'ShelfSpace',  // Update with your app's title
-  description: 'Your application description',  // Update with your app's description
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   console.log('RootLayout rendering');  // Uncomment for debugging
+
+  useEffect(() => {
+    initializeFirebase(); // Call the initializeFirebase function
+  }, []);
+
   return (
-      <html lang="en">
-        <body className={inter.className}>
-          {children}
-        </body>
-      </html>
+    <html lang="en">
+      <Head>
+        <title>ShelfSpace</title>
+        <meta name="description" content="Your application description" />
+      </Head>
+      <body className={inter.className}>
+        <ChakraProvider>
+          <ErrorBoundary>
+            {children}
+          </ErrorBoundary>
+        </ChakraProvider>
+      </body>
+    </html>
   );
 }
